4572dfcdb7afcab0079bf61dfea3874f9892eede
Marco Aurélio Souza Arriate <158333343+Arriatea@users.noreply.github.com>
2025-08-18 22:34:03 -0300

    aula 18/08

--- script.js
+++ script.js
@@ -19,69 +19,106 @@ let posts = [
     }
 ];
 
-window.onload = function(){//executa  a função ao iniciar
-    mostrarPosts()
-    document.querySelector("#postForm").addEventListener("submit", criarPost)//Previne que o formulario seja enviado antes de clicar em submit
-}
-//CREATE
-function criarPost(infosDoEvento){
-    infosDoEvento.preventDefault();//Não joga fora os dados
 
-    // Pega o input dos usuarios 
-    const textoPost = document.querySelector("#postText").value;
-    const categoriaPost = document.querySelector("#postCategory").value;
-    const imagemPost = document.querySelector("#postImage").value;
-    const dataPost = new Date().toLocaleString();
+// Inicialização
+window.onload = function() {
+    carregarPostsLocalStorage();
+    displayPosts();
 
-    const post = {
-        text: textoPost,
-        category: categoriaPost,
-        image: imagemPost,
-        date: dataPost
+    document.querySelector('#postForm').addEventListener('submit', addPost); 
+    document.querySelector("#postList").addEventListener("click", handleClick)
+
+    // localStorage.setItem("nome", "marco")// Guarda um item no armazenamento local
+    // console.log(localStorage.getItem("nome"))// Pega o item
+    // // localStorage.removeItem("nome")
+};
+
+function handleClick(infosDoEvento){
+    const acaoBotao = infosDoEvento.target.dataset.action;
+    const indicePost = infosDoEvento.target.dataset.index;
+    if (!acaoBotao) return;
+
+    if(acaoBotao === "Editar"){
+        editarPost(indicePost)
+    }
+    else if (acaoBotao === "Apagar"){
+        apagarPost(indicePost)
     }
 
-    posts.unshift(post)//Adiciona o post no inicio do array
+}
 
-    mostrarPosts ();
+// Função para exibir os posts
+function displayPosts() {
+    const postList = document.getElementById('postList');
+    postList.innerHTML = '';
+
+    posts.forEach((pegaPost,index) => {
+            const postElement = document.createElement('div');
+            postElement.classList.add('card-post');
+  
+            postElement.innerHTML = `
+                <p>${pegaPost.text}</p>
+                ${pegaPost.image ? `<img src="${pegaPost.image}" alt="Imagem do post" style="max-width:150px;">` : ""}
+                <p><em>Categoria: ${pegaPost.category}</em></p>
+                <p><em>Data e Hora: ${pegaPost.date}</em></p>
+                <button data-action="Editar" data-index = ${index}><i class="fa-solid fa-pen-to-square"></i> Editar</button>
+                <button data-action="Apagar" data-index = ${index}><i class="fa-solid fa-eraser"></i> Apagar</button>
+                <hr style="margin:30px;">`;
+               
+            postList.append(postElement);
+        });
+}
+
+// Função para adicionar um novo post
+function addPost(infosDoEvento) {
+    infosDoEvento.preventDefault();
     
+    const postText = document.getElementById('postText').value;
+    const postCategory = document.getElementById('postCategory').value;
+    const postImage = document.getElementById('postImage').value
+    const postDate = new Date().toLocaleString(); 
+
+    const post = { 
+        text: postText, 
+        category: postCategory, 
+        image: postImage, 
+        date: postDate 
+    };
+    
+    posts.unshift(post);
+    salvarLocalStorage();
+    
+    document.getElementById('postForm').reset();
+    
+    displayPosts();
+}
+// Apagar
+function apagarPost(indicePost){
+    
+    const confirmar = confirm("Deseja realmente excluir este post")
+    if(confirmar){
+        salvarLocalStorage();
+        displayPosts();
+        posts.splice(indicePost,1);}
 }
 
-
-//READ
-function mostrarPosts(){
-    const listaPosts = document.querySelector("#postList")
-    listaPosts.innerHTML = ""
-
-    posts.forEach(pegaItem => {
-        const cardPost = document.createElement("div")
-        cardPost.classList.add("card")
-
-        cardPost.innerHTML = `
-        <p> ${pegaItem.text}</p>
-        <img src = "${pegaItem.image}" style="max-width: 150px"> </img>
-        <p> ${pegaItem.category}</p>
-        <p>${pegaItem.data}</p>
-        <button> Editar </button> 
-        <button> Apagar </button>`
-        
-        listaPosts.append(cardPost)
-
-
-}) 
+//Editar    
+function editarPost(indicePost){
+    const atualizacaoPost =  prompt("Edite o conteudo do seu post!", posts[indicePost].text)
+    posts[indicePost].text = atualizacaoPost
+    salvarLocalStorage();
+    displayPosts()
 }
-//UPDATE
-//DELETE
 
+function salvarLocalStorage(){
+    localStorage.setItem("posts",JSON.stringify(posts))//JSON.stringify tranforma objetos e arrays em string
+}
 
+function carregarPostsLocalStorage (){
+    const postsGuardados = localStorage.getItem("posts")
+    if (postsGuardados){
+        postsGuardados
+        posts = JSON.parse(postsGuardados)
+    }
+}
 
-
-
-
-// Estrurutação de objetos:      
-// const pessoa = {
-//     nome: "Marco",
-//     idade: 18,
-//     cidade: "São Bernardo",
-// }
-// console.log(pessoa.cidade);
-// console.log(pessoa[cidade]);
